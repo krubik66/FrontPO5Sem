61baab5f47508c495351b34afc8c5f251366afa2
krubik66 <krubik66@gmail.com>
2024-01-26 17:48:31 +0100

    doctoredit.py changes

--- Usages/doctoredit.py
+++ Usages/doctoredit.py
@@ -21,18 +21,30 @@ class DoctorEdit:
                command=self.grad_date).grid(row=0, column=2)
 
         self.date = Label(self.root, text="")
-        self.date.grid(column=3, row=0)
+        self.date.grid(column=2, row=1)
 
         self.event_list = Listbox(self.root, selectmode=SINGLE, height=5, width=20)
         self.event_list.bind('<<ListboxSelect>>', self.selected)
 
-        self.delete_button = Button(self.root, text="Wypisz zwolnienie", command=self.on_select)
-        self.delete_button.configure(state="disabled")
-        self.delete_button.grid(column=3, row=1)
-        self.event_list.grid(column=2, row=1)
+        self.write_button = Button(self.root, text="Wypisz zwolnienie", command=self.on_select)
+        self.write_button.configure(state="disabled")
+        self.write_button.grid(column=3, row=1)
+        self.block_button = Button(self.root, text="Zablokuj wydarzenie", command=self.blocking_window)
+        self.block_button.configure(state="disabled")
+        self.block_button.grid(column=3, row=2)
+        self.event_list.grid(column=3, row=0)
 
     def selected(self, event):
-        self.delete_button.configure(state="active")
+        self.write_button.configure(state="active")
+        self.block_button.configure(state="active")
+
+    def blocking_window(self):
+        self.accept_window = Tk()
+        self.accept_window.title("Potwierdzenie")
+
+        self.accept_window.geometry("200x100")
+        Button(self.accept_window, text="Potwierdź", command=self.accept).pack(side=LEFT, expand=YES)
+        Button(self.accept_window, text="Anuluj", command=self.accept_window.destroy).pack(side=RIGHT, expand=YES)
 
     def grad_date(self):
         # wybor dnia -----------------------------------------------
@@ -44,7 +56,8 @@ class DoctorEdit:
         # tu daj eventy ----------------------------------------------------------------------------------
         events = ["Event 1", "Event 2", "Event 3"]
         self.event_list.delete(0, END)
-        self.delete_button.configure(state="disabled")
+        self.write_button.configure(state="disabled")
+        self.block_button.configure(state="disabled")
         for event in events:
             self.event_list.insert(END, event)
 
@@ -74,6 +87,7 @@ class DoctorEdit:
 
     def accept(self):
         self.event_list.delete(self.event_list.curselection())
+        self.accept_window.destroy()
 
 
 if __name__ == "__main__":
